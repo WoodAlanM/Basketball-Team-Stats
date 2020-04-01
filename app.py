a9fbb3df26121a7abfb6057fb8c1a7d290357545
Alan Wood <WoodAlanM@gmail.com>
2020-04-01 10:30:56 -0400

    Added menu functionality

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import constants
 import random
+import sys
 
 
 # Function for creating and returning player lists based on
@@ -54,11 +55,57 @@ def populate_teams(exp_players, inexp_players):
 
     return team1_players, team2_players, team3_players
 
+def display_teams():
+    print("\n")
+    print("1. Panthers")
+    print("2. Bandits")
+    print("3. Warriors")
+    print("\n")
 
 if __name__ == "__main__":
     exp, inexp = create_player_lists()
     panthers_players, bandits_players, warriors_players = populate_teams(exp, inexp)
-    
+    print("Basketball Teams Stats Tool")
+    print("---------------------------")
+    print("Main Menu:")
+    print("\n")
+    print("Please choose an option")
+    print("\n")
+    print("1. Display Team Stats")
+    print("2. Quit")
+    print("\n")
+    not_quit = True
+    in_teams = False
+
+    # Loop for initial menu selection
+    while not_quit:
+        try:
+            selection = int(input("Please make a selection: "))
+        except:
+            print("Please choose only 1 or 2")
+            continue
+        if selection > 2 or selection < 1:
+            print("Please choose either 1 or 2")
+            continue
+        elif selection == 1:
+            display_teams()
+            in_teams = True
+            while in_teams:
+                try:
+                    selection =int(input("Please make a selection: "))
+                except:
+                    print("Please choose only 1,2 or 3")
+                    continue
+                if selection < 1 or selection > 3:
+                    print("Please choose one of the menu options.")
+
+
+        elif selection == 2:
+            not_quit = False
+
+
+
+
 
 
 
