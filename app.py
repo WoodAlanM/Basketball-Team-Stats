7a5e2546d8011506cfd903c13f4b4b057eb23b73
Alan Wood <WoodAlanM@gmail.com>
2020-04-01 11:07:10 -0400

    Should meet expectations

--- app.py
+++ app.py
@@ -62,6 +62,24 @@ def display_teams():
     print("3. Warriors")
     print("\n")
 
+def display_team(team_name, num_players, players_list):
+    print("\n")
+    print("Team: {} stats.".format(team_name))
+    print("---------------------")
+    print("Total players: {}".format(num_players))
+    print("\n")
+    print("Players on team: \n")
+    print(players_list)
+    print("\n")
+
+def get_player_names(players_list):
+    player_names = []
+    for val in players_list:
+        for key, value in val.items():
+            if key == "name":
+                player_names.append(value)
+    return player_names
+
 if __name__ == "__main__":
     exp, inexp = create_player_lists()
     panthers_players, bandits_players, warriors_players = populate_teams(exp, inexp)
@@ -88,9 +106,9 @@ if __name__ == "__main__":
             print("Please choose either 1 or 2")
             continue
         elif selection == 1:
-            display_teams()
             in_teams = True
             while in_teams:
+                display_teams()
                 try:
                     selection =int(input("Please make a selection: "))
                 except:
@@ -98,9 +116,27 @@ if __name__ == "__main__":
                     continue
                 if selection < 1 or selection > 3:
                     print("Please choose one of the menu options.")
-
-
+                    continue
+                if selection == 1:
+                    display_team("Panthers", len(panthers_players), get_player_names(panthers_players))
+                elif selection == 2:
+                    display_team("Bandits", len(bandits_players), get_player_names(bandits_players))
+                elif selection == 3:
+                    display_team("Warriors", len(warriors_players), get_player_names(warriors_players))
+                not_again = True
+                while not_again:
+                    would_continue = input("Would you like to continue? (y)es or (n)o?")
+                    if would_continue == "Y" or would_continue == "y":
+                        not_again = False
+                    elif would_continue == "N" or would_continue == "n":
+                        not_again = False
+                        in_teams = False
+                        not_quit = False
+                        print("Quitting...")
+                    else:
+                        print("Please choose either y for yes or n for no.")
         elif selection == 2:
+            print("Quitting...")
             not_quit = False
 
 
